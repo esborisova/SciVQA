73655e64e79592de0354c0d703c1346e4d6c46ef
Ekaterina Borisova <89642052+esborisova@users.noreply.github.com>
2024-08-12 17:34:36 +0200

    add script for calculating token number average for gemini prompts

---
+++ src/application/calc_aver_token_numb_gemini_prompts.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from vertexai.preview import tokenization
+
+
+def main():
+    df = pd.read_pickle("../../data/scivqa_data_3000.pkl")
+    prompts = pd.read_csv("../../prompts_gemini.csv")
+
+    model_name = "gemini-1.5-flash-001"
+    tokenizer = tokenization.get_tokenizer_for_model(model_name)
+
+    all_tokens = []
+
+    for row, index in prompts.iterrows():
+        for r, i in df.iterrows():
+            prompt = f'Task: {prompts["task"][row]}\nCaption: {df["caption"][r]}\nConstraints:\n{prompts["constraints"][row]}\nOutput Format: {prompts["output_format"][row]}\nExamples: {prompts["examples"][row]}'
+            n_tokens = tokenizer.count_tokens(prompt)
+            all_tokens.append(n_tokens.total_tokens)
+
+    print(
+        "Average token number across the prompts is:",
+        sum(all_tokens) / (len(all_tokens)),
+    )
+
+
+if __name__ == "__main__":
+    main()
