3587af13ad32854e70c3cbb296bf0a9f63909468
Ekaterina Borisova <89642052+esborisova@users.noreply.github.com>
2024-09-04 10:57:49 +0200

    plot dist for the subset of 3K

--- src/utils/plot_papers_dist.py
+++ src/utils/plot_papers_dist.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    df = pd.read_pickle("data/scivqa_data.pkl")
+    df = pd.read_pickle("../../data/scivqa_data_3000_updated_2024-09-01.pkl")
     papers_dist = df.groupby(["year", "venue"]).size().reset_index(name="count")
 
     plt.figure(figsize=(10, 6))
@@ -15,7 +15,7 @@ def main():
     plt.subplots_adjust(bottom=0.2)
     plt.legend(title="Venue", bbox_to_anchor=(1.05, 1), loc="upper left")
     plt.tight_layout()
-    plt.savefig("papers_dist.png")
+    plt.savefig("papers_dist_3000.png")
 
 
 if __name__ == "__main__":
